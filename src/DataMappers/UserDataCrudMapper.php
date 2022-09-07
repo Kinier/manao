c9a0709ad3ea4f309fcc7f248dd3b8b57dce30ed
kinier <govormaxsim@gmail.com>
2022-09-07 23:45:05 +0300

    user data mapper base add

---
+++ src/DataMappers/UserDataCrudMapper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataMappers;
+
+
+class UserDataCrudMapper extends AbstractCrudMapper {
+    protected string $table = 'user.json';
+
+    public static function create(array $data)
+    {
+        
+    }
+
+    public static function find(int $id)
+    {
+
+    }
+
+    public static function delete(int $id)
+    {
+
+    }
+
+    public static function update(int $id, array $data)
+    {
+
+    }
+}
