dbe263846d1abd25a4daa7b25a6e988c0c9b3f80
kinier <govormaxsim@gmail.com>
2022-09-10 21:03:09 +0300

    idk i dont remember

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controllers;
 
+use App\DataMappers\UserDataCrudMapper;
 
-
+use App\Helpers\Request;
 use App\Helpers\Response;
 
 class UserController{
     public static function index()
     {
+
         Response::page('index');
     }
 }
