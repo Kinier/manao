8c671a991811db797596441dd4fbf033699eecbc
kinier <govormaxsim@gmail.com>
2022-09-10 20:59:56 +0300

    forms to components

---
+++ views/components/formLogin.view.php
@@ -0,0 +1,10 @@
+<form method="post" action="/login" id="form_login">
+    <label for="login_form_login">login</label>
+    <input type="text" id="login_form_login" name="login" placeholder="login"/>
+    <p class="error" id="login_form_login_error"></p>
+    <label for="login_form_password">password</label>
+    <input type="password" id="login_form_password" name="password" placeholder="password"/>
+    <p class="error" id="login_form_password_error"></p>
+
+    <input type="submit" value="login" disabled/>
+</form>
