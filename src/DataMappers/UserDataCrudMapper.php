3faabaac26db088e353475179af13866b17990b4
kinier <govormaxsim@gmail.com>
2022-09-10 23:08:44 +0300

    methods add

--- src/DataMappers/UserDataCrudMapper.php
+++ src/DataMappers/UserDataCrudMapper.php
@@ -13,15 +13,16 @@ class UserDataCrudMapper extends AbstractCrudMapper {
 
     public  function create(array $data)
     {
-        $database = json_decode(file_get_contents(self::path. $this->table));
-        array_push($database->users, json_encode($data));
+        $database = json_decode(file_get_contents(self::path. $this->table), true);
+        $data['id'] = count($database['users']);
+        array_push($database['users'], ($data));
         file_put_contents(self::path . $this->table, json_encode($database));
         return true;
     }
     // returns array
     public  function findAll(){
-        $database = json_decode(file_get_contents(self::path. $this->table));
-        return $database->users;
+        $database = json_decode(file_get_contents(self::path. $this->table), true);
+        return $database['users'];
     }
 
     public  function find(int $id)
@@ -31,11 +32,33 @@ class UserDataCrudMapper extends AbstractCrudMapper {
 
     public  function delete(int $id)
     {
+        $database = json_decode(file_get_contents(self::path. $this->table), true);
+        $users = $database['users'];
+        foreach ($users as $key => $user){
+            if ($user['id'] === $id){
+                unset($users[$key]);
+                $database['users'] = $users;
+                file_put_contents(self::path . $this->table, json_encode($database));
+                return true;
+            }
+        }
 
+        return false;
     }
 
     public  function update(int $id, array $data)
     {
+        $database = json_decode(file_get_contents(self::path. $this->table), true);
+        $users = $database['users'];
+        foreach ($users as $key => $user){
+            if ($user['id'] === $id){
+                $users[$key] = array_replace($user, $data);
+                $database['users'] = $users;
+                file_put_contents(self::path . $this->table, json_encode($database));
+                return true;
+            }
+        }
 
+        return false;
     }
 }
