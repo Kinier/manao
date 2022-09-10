db8fdef27089c54814557c7c3c66d03f415020f6
kinier <govormaxsim@gmail.com>
2022-09-10 23:09:04 +0300

    ajax middleware

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,10 @@ $router->get('/', UserController::class, 'index');
 $router->post('/login', AuthController::class, 'login', [
     'middlewareFunction'=>"App\Middleware\AjaxMiddleware::isAjax"
 ]);
-$router->post('/register', AuthController::class, 'register');
+
+$router->post('/register', AuthController::class, 'register', [
+    'middlewareFunction'=>"App\Middleware\AjaxMiddleware::isAjax"
+]);
 $router->get('/logout', AuthController::class, 'logout');
 
 $router->get('/nopage', ErrorController::class, 'noPage');
