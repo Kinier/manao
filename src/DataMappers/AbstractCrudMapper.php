d7334cf790282d1f873603522aa7c5f0a8e7a5f5
kinier <govormaxsim@gmail.com>
2022-09-10 20:56:16 +0300

    static remove

--- src/DataMappers/AbstractCrudMapper.php
+++ src/DataMappers/AbstractCrudMapper.php
@@ -5,11 +5,11 @@ namespace App\DataMappers;
 abstract class AbstractCrudMapper{
     protected const path = __DIR__ . '/../Database/';
 
-    abstract public static function create(array $data);
+    abstract public  function create(array $data);
 
-    abstract public static function find(int $id);
+    abstract public  function find(int $id);
 
-    abstract public static function delete(int $id);
+    abstract public  function delete(int $id);
 
-    abstract public static function update(int $id, array $data);
+    abstract public  function update(int $id, array $data);
 }
